f5900c8c0aad4565b7b9f697f763a12082f3a8ee
lyhe34 <luc.eymard@live.fr>
2024-01-31 16:31:39 +0100

    home page delivery infos shows appropriate min order price for free shipping

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\FishRepository;
+use App\Service\ConfigManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(FishRepository $fishRepository): Response
+    public function index(FishRepository $fishRepository, ConfigManager $configManager): Response
     {
         $featuredFishes = $fishRepository->findBy(['featured' => true]);
 
         return $this->render('home/index.html.twig', [
             'featuredFishes' => $featuredFishes,
+            'freeShipMinCost' => $configManager->getConfig()->getFreeShipMinCost(),
         ]);
     }
 }
